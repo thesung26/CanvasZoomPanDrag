870b02d13cf1e32e75de2753bbbb19e013b415ad
The <thesung96@gmail.com>
2024-04-08 02:24:50 +0700

    init

---
+++ CanvasZoomPanDragLib/Utils/VisualTreeHelperExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace CanvasZoomPanDragLib.Utils
+{
+    public static class VisualTreeHelperExtensions
+    {
+        public static T FindParent<T>(this DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            if (parentObject == null)
+                return null;
+
+            T parent = parentObject as T;
+            if (parent != null)
+                return parent;
+            else
+                return FindParent<T>(parentObject);
+        }
+        public static DependencyObject GetFirstChild(this DependencyObject parent)
+        {
+            if (parent == null)
+                return null;
+
+            int childCount = VisualTreeHelper.GetChildrenCount(parent);
+            if (childCount > 0)
+                return VisualTreeHelper.GetChild(parent, 0);
+            else
+                return null;
+        }
+    }
+}
